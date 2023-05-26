3159ee842f96d0b0d40d1f76445e01f25a22b6b5
Heaplax <1837035764@qq.com>
2023-05-26 15:38:28 +0800

    change dir

--- scripts/apply_vicuna_delta_to_llama.sh
+++ scripts/apply_vicuna_delta_to_llama.sh
@@ -1,1 +1,1 @@
-python -m fastchat.model.apply_delta --base /nobackup/users/zfchen/zt/llama7bhf/models--decapoda-research--llama-7b-hf/  --target /nobackup/users/zfchen/zt/vicuna/vicuna-7b-v0/  --delta /nobackup/users/zfchen/zt/vicuna/vicuna-7b-delta-v0/
+python -m fastchat.model.apply_delta --base /nobackup/users/zfchen/zt/llama7bhf/models--decapoda-research--llama-7b-hf/snapshots/5f98eefcc80e437ef68d457ad7bf167c2c6a1348/  --target /nobackup/users/zfchen/zt/vicuna/vicuna-7b-v0/  --delta /nobackup/users/zfchen/zt/vicuna/vicuna-7b-delta-v0/
