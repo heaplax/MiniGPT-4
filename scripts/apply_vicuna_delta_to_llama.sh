ae164e9e95c45ecd127d4529b563d1a9a6e47969
Heaplax <1837035764@qq.com>
2023-05-26 15:31:03 +0800

    add vicuna sh

---
+++ scripts/apply_vicuna_delta_to_llama.sh
@@ -0,0 +1,1 @@
+python -m fastchat.model.apply_delta --base /nobackup/users/zfchen/zt/llama7bhf/models--decapoda-research--llama-7b-hf/  --target /nobackup/users/zfchen/zt/vicuna/vicuna-7b-v0/  --delta /nobackup/users/zfchen/zt/vicuna/vicuna-7b-delta-v0/
