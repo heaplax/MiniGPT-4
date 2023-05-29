ee26458ac0b12a101826ce9b564d9a98e69c4823
Heaplax <1837035764@qq.com>
2023-05-29 22:24:27 +0800

    add clevr dataset

--- minigpt4/datasets/datasets/clevr_dataset.py
+++ minigpt4/datasets/datasets/clevr_dataset.py
@@ -1,26 +1,79 @@
+import json
 import os
 from PIL import Image
+import torch
 import webdataset as wds
 from minigpt4.datasets.datasets.base_dataset import BaseDataset
-from minigpt4.datasets.datasets.caption_datasets import CaptionDataset
+from collections import OrderedDict
 
 
-class ClevrDataset(CaptionDataset):
+class __DisplMixin:
+    def displ_item(self, index):
+        sample, ann = self.__getitem__(index), self.annotation[index]
+
+        return OrderedDict(
+            {
+                "file": ann["image"],
+                "question": ann["question"],
+                "question_id": ann["question_id"],
+                "answers": "; ".join(ann["answer"]),
+                "image": sample["image"],
+            }
+        )
+
+
+class ClevrDataset(BaseDataset, __DisplMixin):
+
+    def __init__(
+        self, vis_processor=None, text_processor=None, vis_root=None, ann_paths=[]
+    ):
+        """
+        vis_root (string): Root directory of images (e.g. coco/images/)
+        ann_root (string): directory to store the annotation file
+        """
+        self.vis_root = vis_root
+
+        self.annotation = []
+        for ann_path in ann_paths:
+            self.annotation.extend(json.load(open(ann_path, "r"))['questions'])
+
+        self.vis_processor = vis_processor
+        self.text_processor = text_processor
+
+        self._add_instance_ids()
 
     def __getitem__(self, index):
 
         # TODO this assumes image input, not general enough
         ann = self.annotation[index]
 
-        img_file = '{}.jpg'.format(ann["image_id"])
+        img_file = ann["image_filename"]
         image_path = os.path.join(self.vis_root, img_file)
         image = Image.open(image_path).convert("RGB")
 
         image = self.vis_processor(image)
-        caption = ann["caption"]
+        question = ann["question"]
+
+        answer = ann["answer"]
 
         return {
             "image": image,
-            "text_input": caption,
-            "image_id": self.img_ids[ann["image_id"]],
+            "text_input": answer,
+            "question": question,
+        }
+
+    def collater(self, samples):
+        image_list, question_list, answer_list = [], [], []
+
+        num_answers = []
+
+        for sample in samples:
+            image_list.append(sample["image"])
+            question_list.append(sample["text_input"])
+            answer_list.append(sample["answer"])
+
+        return {
+            "image": torch.stack(image_list, dim=0),
+            "text_input": answer_list,
+            "question_split": question_list,
         }
--- minigpt4/processors/blip_processors.py
+++ minigpt4/processors/blip_processors.py
@@ -68,6 +68,39 @@ class BlipCaptionProcessor(BaseProcessor):
         return caption
 
 
+@registry.register_processor("blip_question")
+class BlipQuestionProcessor(BaseProcessor):
+    def __init__(self, max_words=50):
+        self.max_words = max_words
+
+    def __call__(self, question):
+        return self.pre_question(question)
+
+    @classmethod
+    def from_config(cls, cfg=None):
+        if cfg is None:
+            cfg = OmegaConf.create()
+
+        max_words = cfg.get("max_words", 50)
+
+        return cls(max_words=max_words)
+
+    def pre_question(self, question):
+        question = re.sub(
+            r"([.!\"()*#:;~])",
+            "",
+            question.lower(),
+        )
+        question = question.rstrip(" ")
+
+        # truncate question
+        question_words = question.split(" ")
+        if len(question_words) > self.max_words:
+            question = " ".join(question_words[: self.max_words])
+
+        return question
+
+
 @registry.register_processor("blip2_image_train")
 class Blip2ImageTrainProcessor(BlipImageBaseProcessor):
     def __init__(self, image_size=224, mean=None, std=None, min_scale=0.5, max_scale=1.0):
