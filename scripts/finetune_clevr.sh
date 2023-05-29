f0c494b86246e8a1b3c4d8c5dd72786b832fd63e
Heaplax <1837035764@qq.com>
2023-05-29 20:33:36 +0800

    add dis

--- scripts/finetune_clevr.sh
+++ scripts/finetune_clevr.sh
@@ -1,3 +1,18 @@
+source /nobackup/users/zitian/code/Heaplax/anaconda3/bin/activate && conda activate && conda activate minigpt4
+NODE_RANK=${SLURM_PROCID}
+ip2=node${SLURM_NODELIST:5:4}
+NODE_LENGTH=${#SLURM_NODELIST}
+if [[ ${NODE_LENGTH} == 8  ]]; then
+    ip2=node${SLURM_NODELIST:4:4}
+else
+    ip2=node${SLURM_NODELIST:5:4}
+fi
+echo $ip2
+echo $NODE_RANK
+echo $SLURM_JOB_NUM_NODES
 torchrun --nproc_per_node=4 \
+    --master_addr ${ip2} \
+    --node_rank ${NODE_RANK} \
+    --nnodes $SLURM_JOB_NUM_NODES \
     train.py \
     --cfg-path train_configs/minigpt4_stage2_finetune.yaml
