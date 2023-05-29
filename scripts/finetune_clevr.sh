d5053961a01867c28ff5e8a71859ec903eb640fd
Heaplax <1837035764@qq.com>
2023-05-29 20:03:53 +0800

    fix

--- scripts/finetune_clevr.sh
+++ scripts/finetune_clevr.sh
@@ -1,1 +1,1 @@
-torchrun --nproc-per-node 4 train.py --cfg-path train_configs/minigpt4_stage2_finetune.yaml
+torchrun --nproc_per_node 4 train.py --cfg-path train_configs/minigpt4_stage2_finetune.yaml
