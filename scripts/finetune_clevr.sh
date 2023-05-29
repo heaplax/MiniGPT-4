3f9f23c3f4eec79ebb20fadc40edf169365b75bb
Heaplax <1837035764@qq.com>
2023-05-29 20:17:24 +0800

    add master_addr

--- scripts/finetune_clevr.sh
+++ scripts/finetune_clevr.sh
@@ -1,1 +1,4 @@
-torchrun --nproc_per_node 4 train.py --cfg-path train_configs/minigpt4_stage2_finetune.yaml
+torchrun --nproc_per_node 4 \
+    --master_addr 0.0.0.0 \
+    train.py \
+    --cfg-path train_configs/minigpt4_stage2_finetune.yaml
