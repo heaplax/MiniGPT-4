3871e9dafc843e670fa9cf27b2799f8ea356e4b7
Heaplax <1837035764@qq.com>
2023-05-29 20:54:21 +0800

    eee

--- minigpt4/common/dist_utils.py
+++ minigpt4/common/dist_utils.py
@@ -69,6 +69,8 @@ def init_distributed_mode(args):
 
     args.distributed = True
 
+    print(f"args.gpu:{args.gpu}")
+
     torch.cuda.set_device(args.gpu)
     args.dist_backend = "nccl"
     print(
