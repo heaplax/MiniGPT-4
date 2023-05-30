8adbbe458376f78ea1087aa76698487dc73841d7
Heaplax <1837035764@qq.com>
2023-05-30 20:35:14 +0800

    change all to iterloader

--- minigpt4/runners/runner_base.py
+++ minigpt4/runners/runner_base.py
@@ -542,10 +542,10 @@ class RunnerBase:
                     collate_fn=collate_fn,
                     drop_last=True if is_train else False,
                 )
-                loader = PrefetchLoader(loader)
+                # loader = PrefetchLoader(loader)
 
-                if is_train:
-                    loader = IterLoader(loader, use_distributed=self.use_distributed)
+                # if is_train:
+                loader = IterLoader(loader, use_distributed=self.use_distributed)
 
             return loader
 
